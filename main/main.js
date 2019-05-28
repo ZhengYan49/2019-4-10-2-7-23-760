e613a6c41d3d3a539236a35b76d8e9f118dfcd7b
ZhengYan49 <49430782+ZhengYan49@users.noreply.github.com>
2019-05-28 21:07:01 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -1,12 +1,11 @@
-module.exports = function main(s,t) {
+module.exports = function main(inputs) {
     // write your code here...
     var price;
-    var time_price;
-    time_price=0.25*t;
-    if(s<=2) price=7;
-    else if(s>2&&s<=8) price=13;
-    else if(s>8&&t==0) price =18;
-    else if(s>8&&t>0) price=19
+    time_price=0.25*parkTime;
+    if(distance<=2) price=7;
+    else if(distance>2&&distance<=8) price=13;
+    else if(distance>8&&parkTime==0) price =18;
+    else if(distance>8&&parkTime>0) price=19
     else price = 0;
     return "price";
 };
