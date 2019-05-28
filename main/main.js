b929c796473e7588a996c780f1da98c3d77c6c39
ZhengYan49 <49430782+ZhengYan49@users.noreply.github.com>
2019-05-28 20:48:19 +0800

    Update main.js

--- main/main.js
+++ main/main.js
@@ -4,7 +4,7 @@ module.exports = function main(s,t) {
     var time_price;
     time_price=0.25*t;
     if(s<=2) price=6+time_price;
-    else if(s>2&&s<=8) price=4.4+0.8s+time_price;
+    else if(s>2&&s<=8) price=4.4+0.8*s+time_price;
     else if(s>8) price =1.2+1.2*s+time_price;
     else price = 0;
     price=price*10/10;
