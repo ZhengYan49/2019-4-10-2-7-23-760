5b5358b43726391d9c3e07fc6100f3fbba2ad53c
ZhengYan49 <49430782+ZhengYan49@users.noreply.github.com>
2019-05-28 20:55:47 +0800

    Update main-spec.js

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,28 +1,10 @@
 const main = require('../main/main');
 
-describe('taxi fee', function () {
-    it('within 2km and parking' , function() {
-        let inputs = {distance:1,parkTime:5};
-        let summary = main(inputs);
-        let expected = 7;
-        expect(summary).toEqual(expected);
-    });
-    it('between 2km and 8km and parking' , function() {
-        let inputs = {distance:7,parkTime:10};
-        let summary = main(inputs);
-        let expected = 13;
-        expect(summary).toEqual(expected);
-    });
-    it('more than 8km and parking' , function() {
-        let inputs = {distance:10,parkTime:20};
-        let summary = main(inputs);
-        let expected = 18;
-        expect(summary).toEqual(expected);
-    });
-    it('more than 8km and no parking' , function() {
-        let inputs = {distance:15,parkTime:0};
-        let summary = main(inputs);
-        let expected = 19;
-        expect(summary).toEqual(expected);
+describe('main()',() => {
+    it('should calculate the remaindar' , () => {
+        expect(main(1,5).toBe(7));
+        expect(main(7,10).toBe(13));
+        expect(main(10,20).toBe(18));
+        expect(main(15,0).toBe(19));
     });
 });
